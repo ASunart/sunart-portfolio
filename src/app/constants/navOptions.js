e2eb54921eae4ba9a2dfb0970fd458aa19a96230
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2024-03-23 17:26:35 -0500

    Add desktop nav bar options

---
+++ src/app/constants/navOptions.js
@@ -0,0 +1,18 @@
+export const NAV_OPTIONS = [
+    {
+        id: 1,
+        label: "About me"
+    },
+    {
+        id: 2,
+        label: "Experience"
+    },
+    {
+        id: 3,
+        label: "Projects"
+    },
+    {
+        id: 4,
+        label: "Contact"
+    },
+]
