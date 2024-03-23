508cbf56fcbd2ccfa4b0135e0c744dcedf34aac6
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2024-03-23 17:47:29 -0500

    Layout Hero desktop

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -6,29 +6,32 @@ import { SocialBadge } from "./SocialBadge";
 
 export function Hero() {
     return (
-        <section className="flex flex-col items-center relative gap-6 px-4 mt-12 h-[90dvh]">
+        <section className="flex flex-col items-center relative gap-6 px-4 mt-12 h-[90dvh] lg:items-start lg:px-16 lg:flex-row-reverse lg:justify-between">
             <Image
                 src="/points-orange.svg"
                 alt="Points Vector"
                 width={232}
                 height={68}
-                className="absolute -top-20 -left-1/4" />
+                className="absolute -top-20 -left-1/4 lg:top-0 lg:left-0" />
             <Image
                 src="/image_card.webp"
                 alt="Alejandro Solarte Image"
                 width={206}
                 height={206}
-                className="image rounded-full"
+                className="image rounded-full lg:w-[470px]"
             />
-            <div className="flex flex-col gap-4 items-center">
-                <h1 className="font-extrabold text-[46px] text-center text-balance leading-none">Hi there, I&apos;m Alejandro <span className="text-orange">Sunart</span></h1>
-                <PositionSection />
-            </div>
+            <div className="flex flex-col lg:gap-6 lg:h-4/5 lg:justify-center">
+                <div className="flex flex-col gap-4 items-center lg:items-start lg:gap-4">
+                    <h1 className="font-extrabold text-[46px] text-center text-balance leading-none lg:text-[64px] lg:text-left">Hi there, <br className="hidden lg:block" /> I&apos;m Alejandro <span className="text-orange">Sunart</span></h1>
+                    <PositionSection />
+                </div>
 
-            <div className="flex flex-col gap-4 items-center">
-                <p className="text-center mx-auto text-balance"><span className="font-bold">From user insights to digital delight.</span> <br />
-                    Unraveling needs, crafting intuitive experiences.</p>
-                <SocialSection />
+                <div className="flex flex-col gap-4 items-center lg:items-start">
+                    <p className="text-center mx-auto text-balance lg:text-left lg:mx-0 lg:text-xl"><span className="font-bold">From user insights to digital delight.</span> <br />
+                        Unraveling needs, crafting intuitive experiences.</p>
+                    <SocialSection />
+                </div>
+
             </div>
 
             <Image
@@ -36,7 +39,7 @@ export function Hero() {
                 alt="Points Vector"
                 width={232}
                 height={68}
-                className="absolute bottom-0 -right-1/4" />
+                className="absolute bottom-0 -right-1/4 lg:right-0 lg:bottom-12 overflow-clip" />
         </section>
     )
 }
--- src/app/components/SocialBadge.jsx
+++ src/app/components/SocialBadge.jsx
@@ -2,7 +2,7 @@ export function SocialBadge({ label, icon, link }) {
     return (
         <a
             href={link}
-            className="flex justify-center gap-1 w-min items-center px-4 py-[10px] rounded-md bg-white bg-opacity-5 border border-white border-opacity-35 cursor-pointer"
+            className="flex justify-center gap-1 w-min items-center px-4 py-[10px] rounded-md bg-white bg-opacity-5 border border-white border-opacity-35 cursor-pointer lg:text-lg hover:border-light-blue hover:shadow-sm hover:shadow-light-blue"
             target="_blank">
             {icon}
             {label}
