74f9a4b25cb8cfdf914264a39754fae5044d35da
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2024-03-24 13:50:11 -0500

    Add about me section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Hero } from "./components/Hero";
 import { TopBar } from "./components/TopBar";
+import { About } from "./components/About";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
