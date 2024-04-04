12a17e8dca641597847a6812ef5dec4474ce4299
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2024-04-04 11:24:35 -0500

    Add experience constant

--- src/app/components/Experience.jsx
+++ src/app/components/Experience.jsx
@@ -1,14 +1,28 @@
+import { EXPERIENCE } from "../constants/experience"
+
 export function Experience() {
     return (
         <section>
             <h2 className="font-bold text-xl lg:text-4xl">Experience</h2>
-            <article className="bg-gray bg-opacity-5 rounded-xl p-4 border border-orange border-opacity-25 backdrop-blur-[32px]">
-                <span>
-                    <h3>Cargo</h3>
-                    <h4>Empresa</h4>
-                </span>
-                <p>Descripción del trabajo</p>
-            </article>
+            <div className="flex flex-col gap-4">
+                {
+                    EXPERIENCE.map(({id, company, position, description}) => {
+                        return <ExperienceCard key={id} company={company} position={position} description={description} />
+                    })
+                }
+            </div>
         </section>
+    )
+}
+
+function ExperienceCard({ company, position, description }) {
+    return (
+        <article className="bg-gray bg-opacity-5 rounded-xl p-4 border border-orange border-opacity-25 backdrop-blur-[32px]">
+            <span>
+                <h3 className="font-bold text-sm">{position}</h3>
+                <h4 className="font-extralight text-[13px]">{company}</h4>
+            </span>
+            <p className="font-light text-sm text-balance">{description}</p>
+        </article>
     )
 }
