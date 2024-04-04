0f039b4bc9ae5ceb4983fafe4fe6648c810b89ab
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2024-04-04 16:19:52 -0500

    Change footer font sizes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -11,9 +11,9 @@ export function Footer() {
     }
     return (
         <footer className="relative flex justify-around items-center w-full border-t border-white border-opacity-30 p-4">
-            <p className="font-light">{currentYear}</p>
-            <h2 className="font-light">This portfolio is under <span className="text-orange">development</span></h2>
-            <p className="font-light flex cursor-pointer hover:text-orange"
+            <p className="font-light text-sm hidden lg:block lg:text-base">{currentYear}</p>
+            <h2 className="font-light text-sm lg:text-base">This portfolio is under <span className="text-orange">development</span></h2>
+            <p className="font-light cursor-pointer text-sm hidden lg:flex lg:text-base hover:text-orange"
                 onClick={backToTop}>Back to top <ArrowUp01Icon /></p>
         </footer>
     )
