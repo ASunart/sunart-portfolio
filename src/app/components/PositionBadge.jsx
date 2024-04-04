2875dcaf49e97d9a4530c40912b7ce550b57ed38
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2024-04-04 16:05:27 -0500

    Add footer and remove pointer on position badge

--- src/app/components/PositionBadge.jsx
+++ src/app/components/PositionBadge.jsx
@@ -1,7 +1,7 @@
 
 export function PositionBadge({ position }) {
     return (
-        <div className="px-2 py-[6px] bg-orange bg-opacity-20 border-[0.5px] border-orange border-opacity-25 cursor-pointer rounded-xl w-max position-badge lg:text-base">
+        <div className="px-2 py-[6px] bg-orange bg-opacity-20 border-[0.5px] border-orange border-opacity-25 rounded-xl w-max position-badge lg:text-base">
             <p className="font-light text-[12.5px] text-white">{position}</p>
         </div>
     )
